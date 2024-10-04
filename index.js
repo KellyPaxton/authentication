e8e765fce4d4c9a040e715c07648cca4aad86c13
Kelly Paxton <klypxtn@gmail.com>
2024-10-03 21:11:41 -0600

    testing function

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const button = document.getElementById("button")
 
 function test() {
     if (button.style.backgroundColor == "red")
-    button.style.backgroundColor = "ghostwhite";
+    button.style.backgroundColor = '';
     else {
         button.style.backgroundColor = "red";
+        button.style.border = "none";
     }
 }
