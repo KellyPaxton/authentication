730ac5e69b724850af6feaaf7b5e942cb8974403
Kelly Paxton <klypxtn@gmail.com>
2024-10-04 19:03:14 -0600

    resolving issues

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 const button = document.getElementById("button");
 
 function component() {
-    if (button.style.color != 'red'); {
-      button.style.color = 'red
-    } else 
+    if (button.style.color != 'red') {
+      button.style.color = 'red';
+    } else {
       alert('Button is already red');
+    }
   }
