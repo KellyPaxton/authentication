eda157e52ff3414d480cfc1728cfd8277d4a157a
Kelly Paxton <klypxtn@gmail.com>
2024-10-07 16:19:28 -0600

    cleaning up commits in queue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import _ from 'lodash';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
-const button = document.getElementById("button");
+const firebaseConfig = {
+  apiKey: "AIzaSyBGqYPrG49nZuk-yfJfxlKMhWlXpYzH3C8",
+  authDomain: "authentic-ation.firebaseapp.com",
+  projectId: "authentic-ation",
+  storageBucket: "authentic-ation.appspot.com",
+  messagingSenderId: "443024409555",
+  appId: "1:443024409555:web:fed39f451d07fc0a666526",
+  measurementId: "G-EG4WZ6486V"
+};
 
-function component() {
-    if (button.style.color != 'red') {
-      button.style.color = 'red';
-    } else {
-      alert('Button is already red');
-    }
-  }
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
